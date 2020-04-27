e78c34f5ba209dc6fcc082ea88e435b6644e54b3
cs464osu <brian.ozarowicz@gmail.com>
2020-04-27 09:50:51 -0700

    update script feedback

--- HumanRatings/combine_human_ratings.py
+++ HumanRatings/combine_human_ratings.py
@@ -1,8 +1,7 @@
 # Requires Python 3.5 or later
 
 # Assumptions
-#    This script is run in a directory containing 1 or more csv files
-#    Only csv files containing information you want are in this directory
+#    This script is run in a directory containing one or more csv files of human ratings
 
 # Output
 #    three columns, 'Performance', 'Joke', 'HumanScorePostJokeOnly'
@@ -18,12 +17,14 @@ from perf_and_joke_dict import joke, performance
 
 # Get all csv files
 all_files = [i for i in glob.glob('*.csv')]
-print("Combining the files:")
 for f in all_files:
 	if f == "ground_truths_template.csv":
 		all_files.remove(f)
 	else:
-		print("\t", f)
+		pass
+print("Combining these files:")
+for f in all_files:
+	print("\t"+f)
 # Combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_files ], sort=False)
 # Remove notes
@@ -40,3 +41,5 @@ combined_csv.insert(4, 'HumanScore', combined_csv['HumanScorePostJokeOnly'])
 # print to csv
 os.chdir("../MachineLearning")
 combined_csv.to_csv("ground_truth_ratings.csv", index=False)
+dir = str(os.getcwd())
+print("\nground_truth_ratings.csv saved in "+dir)
