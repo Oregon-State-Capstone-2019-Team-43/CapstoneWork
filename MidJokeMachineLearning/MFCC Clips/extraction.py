3d6dc6fcb8ad55a74ac58cc233827d30e800ab33
buitim <buitim@oregonstate.edu>
2020-05-06 04:28:33 -0700

    Touched up python script. Issue with annotations thanks to ELAN. Ask me for details

--- MidJokeMachineLearning/MFCC Clips/extraction.py
+++ MidJokeMachineLearning/MFCC Clips/extraction.py
@@ -1,6 +1,23 @@
 import os
 import sys
 import pandas as pd
+from shutil import rmtree
+
+def checkDirExist(path):
+    """ 
+    This method checks to see if the specified path already exists. 
+    Creates if it does not exist.
+
+    Returns bool
+    """
+    
+    if not os.path.exists(path):
+        print("[+] Directory does not exist")
+        os.makedirs(path)
+        return False
+    else:
+        return True
+
 
 def extract():
     if len(sys.argv) != 2:
@@ -32,21 +49,24 @@ def extract():
     
     print(f"[+] Total Lines: {count}")
 
-    # Check if dir exists
-    if not os.path.exists('exports'):
-        print("[+] Does not exist")
-        os.makedirs('exports')
+    if checkDirExist('exports'):
+        print("[+] Deleting existing exports directory...")
+        rmtree('exports')
+        checkDirExist('exports')
 
     # Run FFMPEG via PowerShell (I'm a filthy windows user at home)
     for i in range(count - 1):
-        inputFilePath = filePaths[i].replace('.eaf', '.mp3')
-        inputFileName = inputFilePath.split('/')[-1].replace('.mp3', '')
-        dirName = inputFilePath.split('/')[7]
-        currStartTime = startTimes[i]
-        # currDuration = durations[i] # Not needed anymore
+                                                                                        # Elan appends 'file:///' to the path...
+        inputFilePath = filePaths[i].replace('.eaf', '.mp3').replace('file:///', '')    # Messy but should get the job done
+        inputFileName = inputFilePath.split('/')[-1].replace('.mp3', '')                # We just want the file name sans extension
+        dirName = inputFilePath.split('/')[4]                                           # Grabs the name of the subdirectory to organize files                                     
+        currStartTime = startTimes[i]                                                   # Grabs the start time of the annotated laughter
+        # currDuration = durations[i]                                                   # Not needed anymore
+
+        checkDirExist(f"./exports/{dirName}")
 
         # os.system(f"powershell.exe ffmpeg -ss {currStartTime} -t {currDuration} -i \'{inputFilePath}\' \'./exports/{inputFileName}_{i}.mp3\'") # Code to run with custon duration
-        os.system(f"powershell.exe ffmpeg -ss {currStartTime} -t 1 -i \'{inputFilePath}\' \'./exports/{dirName}/{inputFileName}_{i}.mp3\'")
+        os.system(f"powershell.exe ffmpeg -ss {currStartTime} -t 1 -i \'{inputFilePath}\' \'./exports/{dirName}/{inputFileName}-{i}.mp3\'")
 
 
 if __name__ == "__main__":
